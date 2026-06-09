abf26518c233283c3cfd87f0ea9f117786014f9e
jacoposd97 <102187045+jacoposd97@users.noreply.github.com>
2026-06-09 08:32:28 +0000

    mission & social link

--- js/links.js
+++ js/links.js
@@ -1,7 +1,7 @@
 const SOCIAL_LINKS = {
   
   instagram: "https://instagram.com/uniagames",
-  facebook: "https://facebook.com/...",
+  facebook: "https://www.facebook.com/share/1AVG83yJFw/?mibextid=wwXIfr",
   discord: "https://discord.com/...",
   linkedin: "https://linkedin.com/..."
 };
